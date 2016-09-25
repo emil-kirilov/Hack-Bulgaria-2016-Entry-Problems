48ffba32c48f310153e76ff760b6b3620722a33e
Emo <emkataburgas@gmail.com>
2016-09-26 00:58:49 +0300

    Comments added

--- lakes.rb
+++ lakes.rb
@@ -1,7 +1,17 @@
 def total_liters
-  depth = 0
-  liters = 0
+  #getting the structure of the lake
   terrain = gets.chomp
+  #total amout of liters on the planet
+  #depth of the lake
+  liters = 0
+  depth = 0
+  
+  #iterating through the structure of the lake
+  #every time we go down and the depth is positive (we are below the water level)
+  #we increase the liters by 500 (the amount added because we go downwards) and 1000 liters more per depth point
+  #every time we move horizontaly 1000 liters per depth point are added (if we are below the water level)
+  #every time we go up and the depth is positive we add 1000 lites per depth 
+  #but we also need to remove 500 liters as we've moved upwards
   terrain.each_char do |c|
     case c
     when 'd'
